cfefb0ffa40329d70f65a063789c9f4d464b1c50
Igor Mouawad <igor@mouawad.com.br>
2021-02-19 16:35:28 -0300

    Delete FundamentosEstruturasDeControleEFunções directory

--- FundamentosEstruturasDeControleEFunções/01 - Fundamentos/01 - HelloWorld.js
+++
@@ -1,6 +0,0 @@
-/*
- *  JS HelloWorld, imprimindo algo no console
- */
-
-//Imprimindo no console a string "HelloWorld"
-console.log("HelloWorld");
--- FundamentosEstruturasDeControleEFunções/02 - Funcoes/15 - IIFE.js
+++
@@ -1,6 +0,0 @@
-//IIFE -> Immediately Invoked Function Expression
-
-(function () {
-    console.log("Sera executado na hora");
-    console.log("Fugindo do escopo mais abrangente");
-})()
